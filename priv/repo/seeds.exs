e0f143393a4ea725d75ea8170e0e62acdc5609df
Onorio Catenacci <catenacci@ieee.org>
2023-11-27 15:20:24 -0500

    Lookup Venue Id
    I've modified the seeds script so I don't have to hard code the venue id value.  This allows me to reorder the venues or add or delete venues without having to modify the back half of the seeds script.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -74,8 +74,32 @@ Pta.Repo.insert!(%Pta.Event.Venue{
   zip: "48226"
 })
 
+# While it may seem odd to add a module in here it allows me to
+# use the id's of the various venues without having to assume any particular
+# id in advance.  As it stands right now, id 1 is Pine Knob Music Theater and id 3 is
+# Michigan Lottery Amphitheatre (formerly Freedom Hill Amphitheatre).  With this little
+# bit of extra code I don't have to worry if I later on need to reorder the venues
+# above or if I have to insert new ones.  The script will continue to work correctly because
+# it will continue to look up the correct id for the venue.
+
+import Ecto.Query
+
+defmodule Lookup do
+  def get_id_for_venue_name(venue_name) do
+    from(v in Pta.Event.Venue,
+      where: v.name == ^venue_name,
+      select: v.id
+    )
+    |> Pta.Repo.one()
+  end
+end
+
+pk_id = Lookup.get_id_for_venue_name("Pine Knob Music Theater")
+# AKA Freedom Hill Amphitheatre
+fh_id = Lookup.get_id_for_venue_name("Michigan Lottery Amphitheatre")
+
 Pta.Repo.insert!(%Pta.Event.Performance{
-  venue_id: 1,
+  venue_id: pk_id,
   name: "Sessanta",
   date: Date.from_iso8601!("2024-05-02"),
   performer: "Primus, Puscifer, and A Perfect Circle",
@@ -83,7 +107,7 @@ Pta.Repo.insert!(%Pta.Event.Performance{
 })
 
 Pta.Repo.insert!(%Pta.Event.Performance{
-  venue_id: 1,
+  venue_id: pk_id,
   name: "Summer Camp With Trucks Tour",
   date: Date.from_iso8601!("2024-06-06"),
   performer: "Hootie & The Blowfish wsg Collective Soul and Edwin McCain",
@@ -91,7 +115,7 @@ Pta.Repo.insert!(%Pta.Event.Performance{
 })
 
 Pta.Repo.insert!(%Pta.Event.Performance{
-  venue_id: 1,
+  venue_id: pk_id,
   name: "Mule Pull '24 Tour",
   date: Date.from_iso8601!("2024-06-11"),
   performer: "Tyler Chilers wsg S. G. Goodman",
@@ -99,7 +123,7 @@ Pta.Repo.insert!(%Pta.Event.Performance{
 })
 
 Pta.Repo.insert!(%Pta.Event.Performance{
-  venue_id: 1,
+  venue_id: pk_id,
   name: "Magic Summer Tour",
   date: Date.from_iso8601!("2024-06-18"),
   performer: "New Kids On The Block wsg Paula Abdul and D. J. Jazzy Jeff",
@@ -107,7 +131,7 @@ Pta.Repo.insert!(%Pta.Event.Performance{
 })
 
 Pta.Repo.insert!(%Pta.Event.Performance{
-  venue_id: 1,
+  venue_id: pk_id,
   name: "The Show",
   date: Date.from_iso8601!("2024-07-10"),
   performer: "Niall Horan",
@@ -115,7 +139,7 @@ Pta.Repo.insert!(%Pta.Event.Performance{
 })
 
 Pta.Repo.insert!(%Pta.Event.Performance{
-  venue_id: 1,
+  venue_id: pk_id,
   name: "Triple Moon Tour",
   date: Date.from_iso8601!("2024-07-16"),
   performer: "Alanis Morissette wsg Joan Jett & the Blackhearts and Morgan Wade",
@@ -123,7 +147,7 @@ Pta.Repo.insert!(%Pta.Event.Performance{
 })
 
 Pta.Repo.insert!(%Pta.Event.Performance{
-  venue_id: 1,
+  venue_id: pk_id,
   name: "Heart & Soul 2024 Tour",
   date: Date.from_iso8601!("2024-07-17"),
   performer: "Earth, Wind & Fire And Chicago",
@@ -131,7 +155,7 @@ Pta.Repo.insert!(%Pta.Event.Performance{
 })
 
 Pta.Repo.insert!(%Pta.Event.Performance{
-  venue_id: 1,
+  venue_id: pk_id,
   name: "Summer of '99 Tour",
   date: Date.from_iso8601!("2024-07-31"),
   performer: "Creed wsg 3 Doors Down and Finger Eleven",
@@ -139,7 +163,7 @@ Pta.Repo.insert!(%Pta.Event.Performance{
 })
 
 Pta.Repo.insert!(%Pta.Event.Performance{
-  venue_id: 1,
+  venue_id: pk_id,
   name: "The Best of All Worlds",
   date: Date.from_iso8601!("2024-08-02"),
   performer: "Sammy Hagar wsg Loverboy",
@@ -147,7 +171,7 @@ Pta.Repo.insert!(%Pta.Event.Performance{
 })
 
 Pta.Repo.insert!(%Pta.Event.Performance{
-  venue_id: 1,
+  venue_id: pk_id,
   name: "Seasons 2024 World Tour",
   date: Date.from_iso8601!("2024-08-06"),
   performer: "Thirty Seconds To Mars wsg AFI, Poppy and Kennyhoopla",
@@ -155,7 +179,7 @@ Pta.Repo.insert!(%Pta.Event.Performance{
 })
 
 Pta.Repo.insert!(%Pta.Event.Performance{
-  venue_id: 1,
+  venue_id: pk_id,
   name: "The Sharp Dressed Simple Man Tour",
   date: Date.from_iso8601!("2024-09-13"),
   performer: "Lynyrd Skynyrd + ZZ Top wsg The Outlaws",
@@ -163,7 +187,7 @@ Pta.Repo.insert!(%Pta.Event.Performance{
 })
 
 Pta.Repo.insert!(%Pta.Event.Performance{
-  venue_id: 3,
+  venue_id: fh_id,
   name: "The CAVES World Tour",
   date: Date.from_iso8601!("2024-05-29"),
   performer: "NEEDTOBREATHE wsg Judah & The Lion",
@@ -171,7 +195,7 @@ Pta.Repo.insert!(%Pta.Event.Performance{
 })
 
 Pta.Repo.insert!(%Pta.Event.Performance{
-  venue_id: 3,
+  venue_id: fh_id,
   name: "Burn It Down Tour 2024",
   date: Date.from_iso8601!("2024-06-08"),
   performer: "Parker McCollum wsg Corey Kent",
@@ -179,7 +203,7 @@ Pta.Repo.insert!(%Pta.Event.Performance{
 })
 
 Pta.Repo.insert!(%Pta.Event.Performance{
-  venue_id: 3,
+  venue_id: fh_id,
   name: "Think Later Tour",
   date: Date.from_iso8601!("2024-07-28"),
   performer: "Tate McRae wsg Presley Regier",
@@ -187,7 +211,7 @@ Pta.Repo.insert!(%Pta.Event.Performance{
 })
 
 Pta.Repo.insert!(%Pta.Event.Performance{
-  venue_id: 3,
+  venue_id: fh_id,
   name: "O.A.R. Summer Tour 24",
   date: Date.from_iso8601!("2024-08-24"),
   performer: "O.A.R. wsg Fitz and The Tantrums",
